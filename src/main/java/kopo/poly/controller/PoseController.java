017ab7eb6514d180da10fdddb510c6a24e9a9f99
tyson0922 <taeseung92l@gmail.com>
2024-08-11 14:07:34 +0900

    pose and quiz both go to the same correct / wrong url so needs fixing

---
+++ src/main/java/kopo/poly/controller/PoseController.java
@@ -0,0 +1,61 @@
+package kopo.poly.controller;
+
+import kopo.poly.dto.object.QuizDTO;
+import kopo.poly.dto.quiz.GradingDTO;
+import kopo.poly.service.IQuizService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class PoseController {
+    private final IQuizService quizService;
+
+//    @GetMapping("face/correct")
+//    public String poseCorrect(){
+//        return  "pose-correct";
+//    }
+//
+//    @GetMapping("face/wrong")
+//    public String poseWrong(){
+//        return "pose-wrong";
+//    }
+
+    @GetMapping("/face/correct")
+    public String poseCorrect(@RequestParam("poseResult") String poseResult, @RequestParam("quizResult") String quizResult, ModelMap model) {
+        model.addAttribute("poseResult", poseResult);
+        model.addAttribute("quizResult", quizResult);
+        return "pose-correct";
+    }
+
+    @GetMapping("/face/wrong")
+    public String poseWrong(@RequestParam("poseResult") String poseResult, @RequestParam("quizResult") String quizResult, ModelMap model) {
+        model.addAttribute("poseResult", poseResult);
+        model.addAttribute("quizResult", quizResult);
+        return "pose-wrong";
+    }
+
+    @GetMapping("pose-estimation")
+    public String poseEstimation(@RequestParam("level") Long level, @RequestParam("number") Long number, ModelMap model) {
+        QuizDTO pDTO = new QuizDTO();
+        pDTO.setLevel(level);
+        pDTO.setQuizNumber(number);
+
+        QuizDTO rDTO = Optional.ofNullable(quizService.getQuiz(level, number)).orElseGet(QuizDTO::new);
+        model.addAttribute("quiz", rDTO);
+        return "pose-estimation";
+    }
+
+    @PostMapping("pose-grading")
+    @ResponseBody
+    public String poseGrading(@RequestBody GradingDTO request) {
+        String grading = request.getGrading();
+        quizService.updateQuizGrading(request);
+        return "Grading received : " + grading;
+    }
+}
+
--- src/main/java/kopo/poly/service/IQuizService.java
+++ src/main/java/kopo/poly/service/IQuizService.java
@@ -1,6 +1,7 @@
 package kopo.poly.service;
 
 import kopo.poly.dto.object.QuizDTO;
+import kopo.poly.dto.quiz.GradingDTO;
 import kopo.poly.dto.request.AnswerDTO;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface IQuizService {
     /**실질적인 비즈니스 로직은 같지만 컨트롤러에서 받는 매개변수의 타입이 달라 코드의 간결함을 위해 함수 오버로딩 하였음.**/
     QuizDTO getQuiz(Long level, Long number);
     QuizDTO getQuiz(AnswerDTO request);
+    int updateQuizGrading(GradingDTO pDTO);
 }
